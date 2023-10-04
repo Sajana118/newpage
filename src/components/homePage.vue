4e946c09efe2834a7257dd05e8ae81e3a9a2f8bf
sajana lama <tmgsajana118@gmail.com>
2023-10-04 17:37:57 +0545

    New page design

---
+++ src/components/homePage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="medicalReport">
+    <div class="navbar">
+      <div class="navbar_content">診療予約</div>
+      <div class="closeButton">
+        <span @click="closePage" class="material-symbols-outlined">close</span>
+      </div>
+    </div>
+    <div class="components">
+      <div class="contentOne">
+        <reservation/>
+      </div>
+      <div class="content_two">hello</div>
+    </div>
+    <div class="footer"></div>
+  </div>
+</template>
+<script>
+import reservation from './reservationPage.vue';
+export default {
+  components: {
+    reservation
+  }
+}
+</script>
+<style>
+.medicalReport{
+  background-color: #b0b0b0;
+  height: 175vh;
+}
+.navbar{
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  height:65px;
+  align-items: center;
+  font-size: 35px;
+  color: black;
+  padding: 15px;
+}
+.components{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 1300px;
+  padding: 36px;
+
+}
+.contentOne{
+  background-color: white;
+  height:1000px ;
+  width: 61%;
+  padding:20px;
+}
+.content_two{
+  background-color: white;
+  width: 36%;
+  height: 700px;
+}
+
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePage from "@/components/homePage.vue";
+import reservation from "@/components/reservationPage.vue";
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'homePage',
+    component: HomePage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/reservationPage',
+    name: 'reservation',
+    component: reservation
+  },
+
 ]
 
 const router = createRouter({
